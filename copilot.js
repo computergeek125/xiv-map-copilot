befcc33da3fe07106ceb17044983eef04abc99ff
computergeek125 <computergeek125@gmail.com>
2023-03-23 21:08:24 -0500

    Added initial SVG overlay support - lots of things got set to position:relative

--- copilot.js
+++ copilot.js
@@ -61,6 +61,7 @@ async function resetTabs() {
         e_content.setAttribute(             "id", e_content_id);
         e_content.setAttribute(           "role", "tabpanel");
         e_content.setAttribute("aria-labelledby", e_button_id);
+        e_content.setAttribute(          "style", "position: relative;");
         const e_tabstrip = document.createElement("div")
         // tabstrip: <ul class="nav nav-tabs" id="myTab" role="tablist">
         e_tabstrip.setAttribute(        "class", "nav nav-tabs");
@@ -72,6 +73,7 @@ async function resetTabs() {
         e_tabcontent.setAttribute(        "class", "tab-content h-100");
         e_tabcontent.setAttribute(           "id", `expac-tabs-${e_id}-tabcontent`);
         e_tabcontent.setAttribute("data-bs-theme", "dark");
+        e_tabcontent.setAttribute(        "style", "position: relative;");
         for (let mx=0; mx<map_index['expansions'][e]["maps"].length; mx++) {
             m = map_index['expansions'][e]["maps"][mx]
             console.log(`Loading map ${m}...`)
@@ -104,20 +106,42 @@ async function resetTabs() {
             map_content.setAttribute(             "id", map_content_id);
             map_content.setAttribute(           "role", "tabpanel");
             map_content.setAttribute("aria-labelledby", map_button_id);
+            map_content.setAttribute(          "style", "position: relative;");
             map_content.setAttribute(       "tabindex", 0);
 
+            const map_svg_id = `map-${e_id}-${map_id}-canvas`;
+            const map_img_id = `map-${e_id}-${map_id}-image`;
             const map_img_relpath = `${e}/${m}/${map_info["filename"]}`;
             const map_img_url = new URL(map_img_relpath, img_url_base);
             //console.log(`${map_img_relpath} | ${map_img_url}`);
             const map_img = document.createElement("img");
-            //map_img.setAttribute("style", "height: 100%; width: 100%; object-fit: contain");
-            //map_img.setAttribute("style", "height: 100%; width: 100%; object-fit: contain");
+            map_img.setAttribute(   "id", map_img_id);
             map_img.setAttribute("class", "img-fluid h-100");
-            map_img.setAttribute("src", map_img_url);
+            map_img.setAttribute("style", "position: absolute;");
+            map_img.setAttribute(  "src", map_img_url);
+            const mi_w = map_img.naturalWidth;
+            const mi_h = map_img.naturalHeight;
+
+            const map_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            map_svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+            map_svg.setAttribute(   "id", map_svg_id);
+            map_svg.setAttribute("class", "img-fluid h-100");
+            map_svg.setAttribute("style", "position: absolute");
+            map_svg.setAttribute("viewBox", `0 0 1177 1177`)
+            //map_svg.setAttribute("viewBox", `0 0 ${mi_w} ${mi_h}`)
+
+            test_cross = gen_svg_cross(100, 100, 50);
+            map_svg.appendChild(test_cross);
 
 
+            //const map_image = document.createElementNS("http://www.w3.org/2000/svg", "image");
+            //map_image.setAttribute(  "id", map_img_id);
+            //map_image.setAttribute("href", map_img_url);
+            
+            //map_svg.appendChild(map_image);
 
             map_content.appendChild(map_img);
+            map_content.appendChild(map_svg);
             e_tabstrip.appendChild(map_tab_li);
             e_tabcontent.appendChild(map_content);
         }
@@ -132,6 +156,34 @@ async function resetTabs() {
     expac_tabs_buttons.firstElementChild.setAttribute(        "class", "nav-link active");
     expac_tabs_buttons.firstElementChild.setAttribute("aria-selected", true);
     expac_tabs_content.firstElementChild.setAttribute(        "class", "tab-pane fade h-100 show active");
+}
+
+function gen_svg_cross(x, y, r, f="red", r_edge=0.1) {
+    // cross: <polygon points="5 0, 25 20, 45 0, 50 5, 30 25, 50 45, 45 50, 25 30, 5 50, 0 45, 20 25, 0 5" fill="red" />
+    //const cross_template = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    //cross_template.setAttribute("points", "5 0, 25 20, 45 0, 50 5, 30 25, 50 45, 45 50, 25 30, 5 50, 0 45, 20 25, 0 5");
+    //cross_template.setAttribute("fill", "red");
+    edge = r*r_edge;
+    offset = r-edge;
+    lr = Math.round(x-r);
+    lo = Math.round(x-offset);
+    le = Math.round(x-edge);
+    tr = Math.round(y-r);
+    to = Math.round(y-offset);
+    te = Math.round(y-edge);
+    rr = Math.round(x+r);
+    ro = Math.round(x+offset);
+    re = Math.round(x+edge);
+    br = Math.round(y+r);
+    bo = Math.round(y+offset);
+    be = Math.round(y+edge);
+    
+
+    const cross = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    //                            top left T    center T     top right T   top right R   center R     bot right R   bot right B   center B     bot left B    bot left L    center L     top left L
+    cross.setAttribute("points", `${lo} ${tr},  ${x} ${te},  ${ro} ${tr},  ${rr} ${to},  ${re} ${y},  ${rr} ${bo},  ${ro} ${br},  ${x} ${be},  ${lo} ${br},  ${lr} ${bo},  ${le} ${y},  ${lr} ${to}`)
+    cross.setAttribute("fill", f);
+    return cross
 }
 
 let index_url;
